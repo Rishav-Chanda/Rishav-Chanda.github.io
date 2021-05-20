80f3c4b4b2902418619fc557b74dad74e73a60a1
Rishav Chanda <rishavchanda0@gmail.com>
2021-05-20 18:09:00 +0530

    website

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -4,10 +4,13 @@ const Home = () => {
     return(
 
         <> 
-        <h1>Welcome Home</h1>
+          <div>
+          <img src="https://firebasestorage.googleapis.com/v0/b/login-ea5e2.appspot.com/o/Developer%20activity.gif?alt=media&token=28c3ec9b-8194-4a00-a491-50ee43b92657" alt=""/>
+          </div>
         </>
 
     );
 };
 
 export default Home;
+
