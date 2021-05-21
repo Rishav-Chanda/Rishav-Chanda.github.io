2e09ccf5c7844affc2e1e0e4488078ebfc3cddad
Rishav Chanda <rishavchanda0@gmail.com>
2021-05-21 20:55:14 +0530

    website

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
 
     <>
-      <div className="container-fluid nav_bg navbar-dark side_padding_bg shadow p-3 mb-5 bg-body rounded h-25">
+      <div className="container-fluid nav_bg navbar-light mt-2">
         <div className='row'>
           <div className="col-10 mx-auto">
 
@@ -16,34 +16,34 @@ const Navbar = () => {
             <nav className="navbar navbar-expand-lg navbar-light navbar-custom ">
               <div className="container-fluid">
                 <a class="navbar-brand " href="#" >
-                  <img src="https://firebasestorage.googleapis.com/v0/b/login-ea5e2.appspot.com/o/20210520_221246.png?alt=media&token=40570207-2e07-4264-b927-d57825470682" alt="" width="20" height="8" class="d-inline-block align-text-top" />
+                  RishavChanda
                 </a>
-
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                   <span class="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavDropdown">
                   <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
-                    <li className="nav-item mr-2 navbar-texts">
+                    <li className="nav-item mr-4 navbar-texts">
                       <NavLink activeClassName="menu_active" exact className="nav-link " aria-current="page" to="/">Home</NavLink>
                     </li>
-                    <li className="nav-item mr-2 ">
+                    <li className="nav-item mr-4 ">
                       <NavLink activeClassName="menu_active" className="nav-link " aria-current="page" to="/about">About</NavLink>
                     </li>
-                    <li className="nav-item mr-2">
+                    <li className="nav-item mr-4">
                       <NavLink activeClassName="menu_active" className="nav-link " aria-current="page" to="/courses">Courses</NavLink>
                     </li>
-                    <li className="nav-item  mr-5">
+                    <li className="nav-item  mr-5 ">
                       <NavLink activeClassName="menu_active" className="nav-link " aria-current="page" to="/contact">Contact</NavLink>
-                    </li>
-                    <li type="button" className="nav-item btn btn-success mr-2  btn-height ">
-                      <NavLink activeClassName="menu_active" className="nav-link " aria-current="page" to="/login">Login</NavLink>
-                    </li>
-                    <li type="button" className="nav-item btn btn-info btn-height" >
-                      <NavLink activeClassName="menu_active" className="nav-link " aria-current="page" to="/singup">SingUp</NavLink>
                     </li>
 
                   </ul>
+                  
+                  <div className="d-flex">
+                    <NavLink className="btn btn-color mr-2 ml-4" to="/login">Login
+                    </NavLink>
+                    <NavLink className="btn btn-color" to="/singup">SingUp
+                    </NavLink>
+                  </div>
                 </div>
               </div>
             </nav>
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,15 +1,49 @@
 import React from "react"
 
+import { NavLink } from "react-router-dom";
+
 const Home = () => {
-    return(
+  return (
 
-        <> 
-          <div>
-          <img src="https://firebasestorage.googleapis.com/v0/b/login-ea5e2.appspot.com/o/Developer%20activity.gif?alt=media&token=28c3ec9b-8194-4a00-a491-50ee43b92657" alt=""/>
+    <>
+      <section id="header" className="d-flex align-items-center mt-5 " >
+           <div className="container-fluid nav_bg">
+          <div className="row">
+            <div className="col-10 mx-auto">
+              <div className="row  mt-3">
+                <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column">
+                  <h1>
+                    Get all <strong className="navbar-brand-big">tutorials</strong> and <strong className="navbar-brand-big">Projects</strong > with <strong className="navbar-brand-big">SourceCode</strong>
+                  </h1>
+                  <h5 className="my-3 text-secondary">
+                    Find all projects and programming tutorials and source codes here from Rishav Chanda. Here all Coding related stuffs are taught.
+                   </h5>
+                   <div className="mt-3">
+                     <NavLink to="/" className="btn btn-color-orange ">
+                       Get Started
+                     </NavLink>
+                     
+                   </div>
+                </div>
+                <div className="col-lg-6 order-1 order-lg-2 header-image">
+                  <img src="https://firebasestorage.googleapis.com/v0/b/login-ea5e2.appspot.com/o/Programming.gif?alt=media&token=55b82912-fbb1-4fb5-a32e-f0de8ba28a84" alt="" />
+
+                </div>
+              </div>
+
+            </div>
+
           </div>
-        </>
 
-    );
+        </div>
+
+      </section>
+      <div>
+
+      </div>
+    </>
+
+  );
 };
 
 export default Home;
